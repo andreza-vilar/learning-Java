5578536b153217652d5418a715837ee01f6b29d1
andreza vilar <andreza.farias@ccc.ufcg.edu.br>
2023-05-16 20:52:17 -0300

    fixing

--- pensionato/Pensionato/src/Quartos.java
+++ pensionato/Pensionato/src/Quartos.java
@@ -4,8 +4,7 @@ public class Quartos {
     private int numeroQuartos;
     private String telefone;
 
-    
-
+   
 
     public Quartos(String names, String emails, String telefone) {
         this.names = names;
@@ -18,50 +17,29 @@ public class Quartos {
         return names;
     }
 
-
-
-
     public void setNames(String names) {
         this.names = names;
     }
-
-
-
 
     public String getEmails() {
         return emails;
     }
 
-
-
-
     public void setEmails(String emails) {
         this.emails = emails;
     }
-
-
-
 
     public int getNumeroQuartos() {
         return numeroQuartos;
     }
 
-
-
-
     public void setNumeroQuartos(int numeroQuartos) {
         this.numeroQuartos = numeroQuartos;
     }
 
-
-
-
     public String getTelefone() {
         return telefone;
     }
-
-
-
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
